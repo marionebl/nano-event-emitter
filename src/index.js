d5c965bb4d900ae34242b48983360a88f324de46
Mario Nebl <hello@herebecode.com>
2016-06-03 18:39:32 +0200

    style: remove useless forEach
    fixes #1

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
-class NanoEventEmitter{
-	constructor() {
-		this.observers = {};
-	}
+class NanoEventEmitter {
+	observers = {};
 
-	on(events, listener) {
-		events.split(' ').forEach((event) => {
+	on(eventNames, listener) {
+		const events = Array.isArray(eventNames) ?
+			eventNames :
+			eventNames.split(' ');
+
+		events.forEach(event => {
 			this.observers[event] = this.observers[event] || [];
 			this.observers[event].push(listener);
 		});
 	}
 
 	off(event, listener) {
-		if (! this.observers[event]) {
+		if (typeof this.observers[event] === 'undefined') {
 			return;
 		}
 
-		this.observers[event].forEach(() => {
-			if (! listener) {
-				delete this.observers[event];
-			} else {
-				var index = this.observers[event].indexOf(listener);
-				if (index > -1) {
-					this.observers[event].splice(index, 1);
-				}
-			}
-		});
+		if (typeof listener === 'undefined') {
+			delete this.observers[event];
+			return;
+		}
+
+		const index = this.observers[event].indexOf(listener);
+
+		if (index > -1) {
+			this.observers[event].splice(index, 1);
+		}
 	}
 
 	emit(event, ...args) {
-		if (! this.observers[event]) {
+		if (typeof this.observers[event] === 'undefined') {
 			return;
 		}
 
-		this.observers[event].forEach(function(observer) {
-			observer(...args);
-		});
+		this.observers[event].forEach(observer => observer(...args));
 	}
 }
 
